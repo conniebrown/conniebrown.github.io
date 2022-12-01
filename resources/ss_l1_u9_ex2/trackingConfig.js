50ee215c75aaa80e6ae2508a145d984e2c5e9838
ConnieBrowm <119605254+ConnieBrowm@users.noreply.github.com>
2022-12-01 15:36:15 +0100

    TEST

---
+++ resources/ss_l1_u9_ex2/trackingConfig.js
@@ -0,0 +1,10 @@
+var TrackingConfig = {
+	"type" : "scorm_2004",
+        "mastery_score": "80",
+        "success_status": "passed",
+        "unsuccess_status": "failed",
+        "send_score":"true",
+        "send_status":"true",
+        "send_interactions":"true",
+        "appversion"  : "2.3.0"
+};
